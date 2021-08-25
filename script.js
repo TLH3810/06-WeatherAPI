fa2dd03f5136a39dc5a2e8a908405bf24af1633b
Yorkie Fan <terahowell@gmail.com>
2021-08-24 20:19:51 -0400

    pushing code able to get coconsol log working

---
+++ script.js
@@ -0,0 +1,112 @@
+
+
+var weatherApiUrl = 'https://api.openweathermap.org';
+var weatherApiKey = '357c53761ecc7f149f068d3e1d074e7d';
+//var fiveDayApi = `https://api.openweathermap.org/api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${weatherApiKey}`;
+
+//var dailyApi = data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${weatherApiKey};
+
+var searchHistory = [];
+
+//download from fivedayapi
+//creating variables for the data from the JSON (current day, latitude, longitude, current humidity,windspeed,temp, icon )
+
+//use lat and long from 5dayapi as input for daily api (give current uvi increment i++ for loop for 5 days)
+
+//function for retrieving the current weather data from the OpenWeather API
+/////store in veriables the fetch data
+
+
+let citySearch = document.querySelector("#search-city");
+let submitSearch = document.querySelector("#submit-search");
+let cityName = ""
+
+/* submitSearch.click(function () {
+    event.preventDefault();
+    cityName = citySearch.value;
+    getWeatherApi(cityName)
+}), */
+
+    function returnWeatherForecast(event) {
+        //make URL
+        console.log("api")
+        var fiveDayApi = `https://api.openweathermap.org/api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${weatherApiKey}`;
+        fetch(fiveDayApi)
+
+            .then(function (response) {
+              return response.json();
+
+            })
+            .then(function (locationResult){
+                console.log(locationResult);
+            })
+            .catch(function (error){
+                console.error(error);
+            });
+
+
+    }
+
+submitSearch.addEventListener("click",
+async function () {
+    cityName = citySearch.value;
+    console.log(cityName);
+    returnWeatherForecast(cityName);
+})
+/* let forecastInfo = response.list;
+
+function getWeatherApi(cityName) {
+    let fiveDayApi = `https://api.openweathermap.org/api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${weatherApiKey}`;
+   fetch(fiveDayApi)
+   .then(function (res){
+       console.log(res.json());
+
+    })
+    .then(function (data){
+        console.log(city,data);
+    })
+    .catch(function (err){
+        console.log(err);
+    });
+}  */
+
+
+/*
+function displayCityHistory(){
+    console.log('displayHistory')
+    let searchHistory = document.querySelector('searchHistory');
+    searchHistory.innerHTML = '';
+    for (var i = searchHist.length -1; i >=0; i--){
+        var button = document.createElement('button');
+        button.setAttribute('type','button');
+        button.setAttribute('data-search', searchHist[i]);
+        button.classList.add('history-button','button-history')
+        button.textContent=searchHist[i];
+
+        console.log(searchHist);
+        console.log(searchHistory);
+
+    }
+} */
+
+
+
+
+/* function getDailyWeather(location) {
+    var { lat } = location;
+    var { lon } = locaiton;
+    var city = location.name
+
+    //var dailyAPI = api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${weatherApiKey}
+
+
+    fetch(dailyAPI)
+        .then(function (res) { return res.json(); })
+        .then(function (data) { renderItems(city, data); })
+        .catch(function (err) { console.error(err); })
+
+}
+// function for the history list to display from most recent
+// function needed to store update local storage history before display
+// function to get the search history from local storag
+*/
